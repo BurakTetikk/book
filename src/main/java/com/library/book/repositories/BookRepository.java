be8b83839e55df6e7e79348be90e298f6f9951de
BurakTetikk <burktetk@gmail.com>
2024-07-03 15:54:53 +0300

    staj calisma

--- src/main/java/com/library/book/repositories/BookRepository.java
+++ src/main/java/com/library/book/repositories/BookRepository.java
@@ -7,10 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitleContaining(String title);
-    List<Book> findByAuthorContaining(String title);
-    List<Book> findByUser(User user);
-    List<Book> findByUserAndTitleContaining(User user, String title);
+    List<Book> findByTitleContaining(
+            String title
+    );
+    List<Book> findByAuthorContaining(
+            String title
+    );
+    List<Book> findByUser(
+            User user
+    );
+    List<Book> findByUserAndTitleContaining(
+            User user,
+            String title
+    );
 
 
 }
