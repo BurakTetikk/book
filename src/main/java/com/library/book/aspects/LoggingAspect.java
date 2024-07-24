a4b7a793aa50c33016987bd963de5495f6003fad
BurakTetikk <burktetk@gmail.com>
2024-07-24 15:03:02 +0300

    AOP and Log

--- src/main/java/com/library/book/aspects/LoggingAspect.java
+++ src/main/java/com/library/book/aspects/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.library.book.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* com.library.book.services.*.*(..))")
+    //Controller katmanının tüm methodlarından önce loglama yapar
+    @Before("execution(* com.library.book.controllers.*.*(..))")
     public void logBeforeMethodExecution(JoinPoint joinPoint) {
         System.out.println("Executing method: " + joinPoint.getSignature());
     }
+
+    //Service katmanının tüm metodlarından sonra loglama yapar ve sonucu döner
+    @AfterReturning(value = "execution(* com.library.book.services.*.*(..))", returning = "result")
+    public void logAfterMethodExecution(JoinPoint joinPoint, Object result) {
+        System.out.println("Executing method: " + joinPoint.getSignature().getName() + ", Result: " + result.toString());
+    }
+
 }
