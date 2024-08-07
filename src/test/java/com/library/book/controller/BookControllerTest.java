00e757b6af1db2bb57ab33ecff236fecfabb73b1
BurakTetikk <burktetk@gmail.com>
2024-08-07 11:46:24 +0300

    Unit Test

--- src/test/java/com/library/book/controller/BookControllerTest.java
+++ src/test/java/com/library/book/controller/BookControllerTest.java
@@ -1,52 +1,36 @@
 package com.library.book.controller;
 
-import com.library.book.controllers.BookController;
 import com.library.book.dto.BookDto;
 import com.library.book.entity.BookEntity;
-import com.library.book.entity.UserEntity;
-import com.library.book.mapper.MapperUtil;
+import com.library.book.repositories.BookRepository;
 import com.library.book.services.BookService;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RequiredArgsConstructor
-@WebMvcTest(BookController.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 public class BookControllerTest {
 
-    //@Autowired
-    private final MockMvc mockMvc;
+    @Mock
+    private BookRepository bookRepository;
 
-    @MockBean
+    @InjectMocks
     private BookService bookService;
 
-/*
-    @MockBean
-    private MapperUtil mapperUtil;*/
 
 
     @Test
     public void testGetBookById() throws Exception {
-/*        BookEntity bookEntity = new BookEntity(
-                87L,
-                "Test Book",
-                "Test Author",
-                "98765345678",
-                1687,
-                12,
-                null
-        );*/
-
-        //BookDto bookDto = mapperUtil.convert(bookEntity, new BookDto());
-
-        BookDto bookDto = new BookDto(
+        BookEntity mockBook = new BookEntity(
                 87L,
                 "Test Book",
                 "Test Author",
@@ -56,13 +40,14 @@ public class BookControllerTest {
                 null
         );
 
-        given(bookService.getBookById(87L)).willReturn(bookDto);
+        when(bookRepository.findBookById(87L)).thenReturn(mockBook);
 
-        mockMvc.perform(get("/api/books/87"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Test Book"));
+        BookDto result = bookService.getBookById(87L);
 
-
+        assertNotNull(result);
+        assertEquals(87L, result.getId());
+        assertEquals("Test Book", result.getTitle());
+        verify(bookRepository).findBookById(87L);
 
     }
 }
